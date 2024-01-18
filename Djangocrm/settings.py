acf6a026f979ef56ae19f2f97ad323379b50f8ae
bloszom <bloszomgianem@gmail.com>
2024-01-17 22:42:12 -0600

    changes

--- Djangocrm/settings.py
+++ Djangocrm/settings.py
@@ -110,6 +110,8 @@ DATABASES = {
        # 'PORT': env("DB_PORT"),
     }
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
--- Leads/models.py
+++ Leads/models.py
@@ -1,7 +1,9 @@
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db.models.signals import post_save
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
 
 class User(AbstractUser):
@@ -50,13 +52,13 @@ class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
-    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True)
     agent = models.ForeignKey("Agent", null=True, on_delete=models.SET_NULL)
     category = models.ForeignKey("Category", related_name="leads", null=True, blank=True, on_delete=models.SET_NULL)
-    description = models.TextField()
-    date_added = models.DateTimeField(auto_now_add=True, default=timezone.now)
-    phone_number = models.CharField(max_length=20)
-    email = models.EmailField()
+    description = models.TextField(default='Your default value here')
+    date_added = models.DateTimeField(default=timezone.now)
+    phone_number = models.CharField(max_length=20, null=True, default=None)
+    email = models.EmailField(default='example@example.com')
     profile_picture = models.ImageField(null=True, blank=True, upload_to="profile_pictures/")
     converted_date = models.DateTimeField(null=True, blank=True)
 
