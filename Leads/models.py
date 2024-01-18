24b421d32c23bfa1e93e8ebfaebaa95a92cd47d8
bloszom <bloszomgianem@gmail.com>
2024-01-17 21:20:24 -0600

    models modifications

--- Leads/models.py
+++ Leads/models.py
@@ -54,7 +54,7 @@ class Lead(models.Model):
     agent = models.ForeignKey("Agent", null=True, on_delete=models.SET_NULL)
     category = models.ForeignKey("Category", related_name="leads", null=True, blank=True, on_delete=models.SET_NULL)
     description = models.TextField()
-    date_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True, default=timezone.now)
     phone_number = models.CharField(max_length=20)
     email = models.EmailField()
     profile_picture = models.ImageField(null=True, blank=True, upload_to="profile_pictures/")
@@ -82,7 +82,7 @@ class FollowUp(models.Model):
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE, default='Default Organization')
 
     def __str__(self):
         return self.user.email
